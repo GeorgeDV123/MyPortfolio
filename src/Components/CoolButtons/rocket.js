56d355fa6f4b18b448eab0e19d16802178fc823e
GeorgeDV123 <115605263+GeorgeDV123@users.noreply.github.com>
2023-01-07 14:31:35 +1100

    hotfix

--- src/Components/CoolButtons/rocket.js
+++ src/Components/CoolButtons/rocket.js
@@ -1,10 +1,5 @@
 export function Rocket() {
-  const speed = Math.floor(Math.random() * 5000 + 1000);
-
-  for (let i = 0; i < 3; i++) {
-    var rocket = document.createElement("span");
-    rocket.classList.add("rocket");
-    rocket.style.animationDuration = `${speed}ms`;
-    document.body.append(rocket);
-  }
+  var rocket = document.createElement("span");
+  rocket.classList.add("rocket");
+  document.body.append(rocket);
 }
